a2d9cc9ee8600d462990c788d0a755d3fc9a9855
Syed Arham Hussain <syedarhamhussain29@gmail.com>
2023-08-17 16:49:27 +0500

    update: getting modified files

--- util/update.js
+++ util/update.js
@@ -17,15 +17,12 @@ const getModifiedFiles = () => {
     `Untracked files:`
   ];
   const indexOf = [gitCode.indexOf(tempArr[0]), gitCode.indexOf(tempArr[1])];
-  return (
-    gitCode.indexOf(tempArr[0]) &&
-    String(gitCode.slice(indexOf[0] + tempArr[0].length, indexOf[1]))
-      .replace(/modified:/g, "")
-      .replace(/\t/g, "")
-      .replace(/ /g, "")
-      .trim()
-      .split("\n")
-  );
+  return String(gitCode.slice(indexOf[0] + tempArr[0].length, indexOf[1]))
+    .replace(/modified:/g, "")
+    .replace(/\t/g, "")
+    .replace(/ /g, "")
+    .trim()
+    .split("\n");
 };
 
 module.exports = GitUpdate;
